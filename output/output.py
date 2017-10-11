d5e1949e5c146577ff79d2a86cd800de93c08363
Hwayong Jang <idgaid92320@naver.com>
2017-10-11 21:17:31 +0900

    Project v.1

---
+++ output/output.py
@@ -0,0 +1,16 @@
+# output.py >> 추출한 데이터를 그래프로 그려주고 이미지 파일로 저장
+
+'''
+from mzXML_analyzer import ~ (.mzXML 확장자를 부넉하게 해 주는 패키지)
+import matplotlib (꼭 matplotlib이 아니더라도 데이터를 플로팅 해주는 패키지
+'''
+
+# Step4-1. Base Peak Chromatogram plotting and save as image file
+'''matplotlib 등의 함수를 이용해 그래프를 출력하고 이미지 파일로 저장'''
+def plot_and_save_BPC() :
+    print("Step4-1. Base Peak Chromatogram 플로팅과 저장 완료!")
+
+# 4-2. Mass Spectrum plotting and save as image file
+'''matplotlib 등의 함수를 이용해 그래프를 출력하고 이미지 파일로 저장'''
+def plot_and_save_MS() :
+    print("Step4-2. Mass Spectrum 플로팅 완료!\n")
