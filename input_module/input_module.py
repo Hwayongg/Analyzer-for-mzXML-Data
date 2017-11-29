9fe05a7e2503a04d83f588b4e5b9a5c5a903fb76
Hwayong Jang <idgaid92320@naver.com>
2017-11-29 18:18:25 +0900

    2017.11.29

--- input_module/input_module.py
+++ input_module/input_module.py
@@ -1,31 +1,43 @@
-# -*- coding: euc-kr -*-
+# -*- coding: utf-8 -*-
 # Made by HY.Jang
 # input_module.py >> .mzXMLÆÄÀÏÀ» ÆÄÀÌ½ã¿¡ ·ÎµåÇØÁÜ
 '''
-from pyopenms import ~ (.mzXML È®ÀåÀÚ¸¦ ¿­ °Ô ÇØÁÖ´Â ÆÐÅ°Áö)
+from pyopenms import ~ (.mzXML)
 '''
 
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from library.pyOpenMS.pyopenms import *
+
+
+'''
+import ..library.pyopenms
+
+
+pyopenms.__version__
 # 2. pyopenms¸¦ ÀÌ¿ë, mzXMLÆÄÀÏÀ» ÆÄÀÌ½ã¿¡ ·Îµå
 class file_loader:
     file_path = ''
     def __init__(self):
-        print("¦® 2. ÇÁ·Î±×·¥¿¡ Áú·®ºÐ¼® µ¥ÀÌÅÍ ºÒ·¯¿À±â")
+        print("")
     def input_path(self):
-        self.file_path = input("¦­ >>> mzXMLÆÄÀÏÀÇ °æ·Î¸¦ ÀÔ·ÂÇØÁÖ¼¼¿ä: ")
+        self.file_path = input("")
         if self.file_path == 'sample':
             self.file_path = 'G:\Python\sample\sample.mzXML'
-        print("¦­ >>> ÀÔ·ÂµÈ °æ·Î´Â [%s] ÀÔ´Ï´Ù." %self.file_path)
+        print("" %self.file_path)
     def file_open(self):
-        print("¦± >>> Complete!\n\n")
-
-# ÆÄÀÏ ÆÐ½º¸¦ Àß¸ø ÀÔ·ÂÇßÀ» °æ¿ì¿¡ ´Ù½Ã ÀÔ·ÂÇÏµµ·Ï ÇÏ´Â ÄÚµå¸¦ ³Ö¾îº¸¸é ¾î¶³±î?
+        print(" >>> Complete!\n\n")
 
 
 
 
+'''
 
 
+'''
 if __name__=="__main__":
     file_path = 'H:\Python\sample.mzXML'
     print('''##### sample의 file path를 입력 #####
-##### path: %s #####''' %file_path)
+##### path: %s #####''' %file_path)'''
